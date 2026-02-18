d6556a80bd97067400d6afaaf0ef41d818ddd5c9
Cirarrazavalm <clemente.irarrazaval@gmail.com>
2026-02-17 21:38:10 -0800

    Added sign up page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,34 @@
-// app/login/page.tsx
 "use client";
+
+import { useState } from "react";
+import Link from "next/link";
+import { supabase } from "@/lib/supabase/supabaseClient";
 
 import Image from "next/image";
 import "./signup.css"; // only works if this CSS is treated as global; if it errors, use Option B below
 
 export default function LoginPage() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: email,
+      password: password,
+    });
+
+    if (error) {
+      alert(error.message);
+      console.log(error);
+      return;
+    }
+
+    console.log("Logged in!", data);
+  };
+
   return (
+
     <div className="signup-page">
       <div className="login-card">
         {/* Logo */}
@@ -23,13 +46,26 @@ export default function LoginPage() {
         <h1 className="login-title">Welcome back!</h1>
 
         {/* Form */}
-        <label className="login-label">Username</label>
-        <input className="login-input" type="text" />
+        <label className="login-label">Email</label>
+        <input
+        className="login-input"
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+/>
+       <label className="login-label">Password</label>
+        <input
+          className="login-input"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-        <label className="login-label">Password</label>
-        <input className="login-input" type="password" />
+        <button className="login-button" type="button" onClick={handleLogin}>
+  Log In
+</button>
+<Link href="/signup">Create an account</Link>
 
-        <button className="login-button">Log In</button>
 
         {/* Footer link */}
         <p className="login-help">
